5632e49a8ae9e2209cce55e2081c584ea419e3ed
Richard Knop <risoknop@gmail.com>
2016-04-06 12:27:20 +0100

    Updated code to get rid of deprecated warnings.

--- oauth2server/proj/settings/default.py
+++ oauth2server/proj/settings/default.py
@@ -34,23 +34,28 @@ STATICFILES_DIRS = (
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'proj', 'templates'),
-)
-TEMPLATE_CONTEXT_PROCESSORS = (
-    # 'django.contrib.auth.context_processors.auth',
-    # 'django.core.context_processors.debug',
-    # 'django.core.context_processors.i18n',
-    # 'django.core.context_processors.media',
-    # 'django.core.context_processors.request',
-    'django.core.context_processors.static',
-    # 'django.contrib.messages.context_processors.messages',
-    # 'django.core.context_processors.request',
-)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'proj', 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                # 'django.contrib.auth.context_processors.auth',
+                # 'django.template.context_processors.debug',
+                # 'django.template.context_processors.i18n',
+                # 'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                # 'django.template.context_processors.tz',
+                # 'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
